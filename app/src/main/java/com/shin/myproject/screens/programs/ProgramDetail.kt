dcf24462c8f7bda2a75a46e11d52c5b324d0a8e4
Kenneth Laurence <kbonaagua2021@student.nbscollege.edu.ph>
2023-11-22 22:14:41 +0800

    update of Add Subject Screen

--- app/src/main/java/com/shin/myproject/screens/programs/ProgramDetail.kt
+++
@@ -1,7 +0,0 @@
-package com.vpmobiledev.nbscollege.screens.programs
-
-data class ProgramDetail(
-    val name: String,
-    val fullDetail : String,
-    val careers: List<String>,
-)
--- app/src/main/java/com/shin/myproject/screens/programs/ProgramList.kt
+++
@@ -1,7 +0,0 @@
-package com.vpmobiledev.nbscollege.screens.programs
-
-data class ProgramList(
-    val name: String,
-    val shortDescription: String,
-    val route: String,
-)
